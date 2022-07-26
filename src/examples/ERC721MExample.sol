92e5dd15433c93ff89f9a890fd5281e857f9af97
0xphaze <0xphaze@gmail.com>
2022-07-26 14:20:24 +0200

    meh

--- src/examples/ERC721MExample.sol
+++ src/examples/ERC721MExample.sol
@@ -1,12 +1,6 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// import "@openzeppelin/contracts/access/Ownable.sol";
-// import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-// import "@openzeppelin/contracts/access/AccessControl.sol";
-// import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-
-import "openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "solmate/auth/Owned.sol";
 import "solmate/utils/LibString.sol";
 
